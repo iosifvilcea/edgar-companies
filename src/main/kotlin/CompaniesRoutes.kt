bcd1fe2548b8f05bad63b4410a8223814fde82da
iosifvilcea <iosifvilcea@protonmail.com>
2025-08-09 03:13:06 +0300

    Moved fetching companies logic to ActiveCompaniesSyncWorker which will be fired off by a cronjob.

--- src/main/kotlin/CompaniesRoutes.kt
+++ src/main/kotlin/CompaniesRoutes.kt
@@ -1,12 +1,29 @@
 package com.blankthings
 
+import ActiveCompaniesSyncWorker
+import io.ktor.http.HttpStatusCode
+import io.ktor.server.response.respond
 import io.ktor.server.routing.*
+import java.lang.Exception
 
 fun Route.companiesRoutes(companyRepository: CompanyRepository) {
-    route("companies") {
+    route("/companies") {
         get {
             val activeCompanies = companyRepository.getActiveCompanies()
             println("Fetched ActiveCompanies: $activeCompanies")
+        }
+    }
+
+    // TODO - This route should be secured and not accessible by the public.
+    route("/internal") {
+        post("/sync-companies") {
+            try {
+                val syncWorker = ActiveCompaniesSyncWorker(companyRepository = companyRepository)
+                syncWorker.syncActiveCompanies()
+                call.respond(HttpStatusCode.OK, "Sync completed")
+            } catch (e: Exception) {
+                call.respond(HttpStatusCode.InternalServerError, "Sync failed: ${e.message}")
+            }
         }
     }
 }
--- src/main/kotlin/CompanyRepository.kt
+++ src/main/kotlin/CompanyRepository.kt
@@ -2,4 +2,8 @@ package com.blankthings
 
 interface CompanyRepository {
     suspend fun getActiveCompanies(): List<ActiveCompany>
+    suspend fun addCompany(activeCompany: ActiveCompany)
+    suspend fun addCompanies(activeCompanies: List<ActiveCompany>)
+    suspend fun deleteCompany(activeCompany: ActiveCompany)
+    suspend fun updateCompanies(activeCompanies: List<ActiveCompany>)
 }
